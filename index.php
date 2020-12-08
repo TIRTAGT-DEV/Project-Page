19e0c2bc2d98014dc945c705a863c47374888cf4
Matthew Tirtawidjaja <matthew@tirtagt.xyz>
2020-12-08 23:45:39 +0700

    Temporary index.php
    Atleast it's not a 404 blank page !

---
+++ index.php
@@ -0,0 +1,75 @@
+<html lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<meta property="og:type" content="website" />
+		<meta property="og:site_name" content="TIRTAGT Developing Team Projects" />
+		<meta property="og:title" content="TIRTAGT Developing Team - Home" />
+		<meta name='author' content='TIRTAGT Developing Team' />
+		<meta property="og:description" content="Check out our projects !" />
+		<meta property="og:image" content="" />
+		<meta property="og:url" content="https://project.tirtagt.xyz/" />
+		<link rel="stylesheet" type="text/css" href="https://cdn.tirtagt.xyz/SAMUTO/css/GlobalUI.css" />
+        <link rel="icon" href="https://cdn.tirtagt.xyz/VenonNation/favicon/png/" type="image/png" media="print" onload="this.media='all'">
+        <link rel="stylesheet" type="text/css" href="https://cdn.tirtagt.xyz/SAMUTO/css/MobileUI.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.tirtagt.xyz/SAMUTO/css/DesktopUI.css" />
+		<title>TIRTAGT Developing Team - Home</title>
+	</head>
+	
+<body>
+	<div class="Container">
+	  <div class="Header" id="HeaderContainer">
+        <a href="https://tirtagt.xyz/"><img src="https://cdn.tirtagt.xyz/THome/TIRTAGT_white.png" style="width: 190px; height: 50px;"></img></a>
+        <button class="TopNavToogle" style="display: none;" onclick="OpenTopNav()">&#9776;</button>
+		<ul id="TopNavContainer">
+            <li><a href="#" style="margin-right: 0px;" class="NavButtonText">Home</a></li>
+            <div id="NewSeparator"></div>
+			<li><a href="#" style="margin-right: 0px;" class="NavButtonText">Projects List</a></li>
+			<div id="NewSeparator"></div>
+			<li><a href="#" style="margin-right: 0px;" class="NavButtonText">Contact us</a></li>
+			<div id="NewSeparator"></div>
+			<li><a href="#" style="margin-right: 0px;" class="NavButtonText">About us</a></li>
+        </ul>
+      </div>  
+	  <div class="Separator"></div>
+	  <div class="Content">
+	        <center>
+	            Hello, The page is still under-construction. <br>
+				Please came again later !<br><br>
+				<span class="Button"><a href="https://discord.gg/GJjQ3at">Join our Discord Server.</a></span><br><br>
+			</center>
+	  </div>
+	  
+	  <div class="Footer">
+	    <center>
+		    Copyright © 2020 TIRTAGT Developing Team
+	    </center>
+	  </div>
+	</div>
+
+<!--Start Cookie Script-->
+
+<!-- Google Analytics -->
+<script type="text/plain" cookie-consent="tracking" data-ad-client="ca-pub-3439202890923584" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- end of Google Analytics-->
+
+<!-- Mobile Navigation Bar -->
+<script type="text/plain">
+function OpenTopNav() {
+var NavVar = document.getElementById("TopNavContainer");
+var HeaderConfig = document.getElementById("HeaderContainer");
+	if (NavVar.style.display === "block") {
+		NavVar.style.display = "none";
+		HeaderConfig.style.height = "70px";
+		
+	} else {
+		NavVar.style.display = "block";
+		HeaderConfig.style.height = " 100vh";
+	}
+}
+</script>
+<!-- end of Mobile Navigation Bar-->
+
+<!--End Cookie Script-->
+</body>
+</html>
